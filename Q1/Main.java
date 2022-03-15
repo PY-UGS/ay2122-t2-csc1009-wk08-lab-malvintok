cf836f0ffae83fa989f748221678b6f1e4dc716c
Malvin Tok <93638561+malvintok@users.noreply.github.com>
2022-03-15 11:33:44 +0800

    Update Main.java

--- Q1/Main.java
+++ Q1/Main.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input radius: ");
-        Double area = input.nextDouble();
-        CircleWithException test = new CircleWithException(area);
-        System.out.println("Area: " + test.getArea());
-        System.out.println("Diameter: " + test.getDiameter());
-        input.close();
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Input radius: ");
+            Double area = input.nextDouble();
+            CircleWithException test = new CircleWithException(area);
+            System.out.println("Area: " + test.getArea());
+            System.out.println("Diameter: " + test.getDiameter());
+            input.close();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
